e505e6e4c9d3c9ab56ea8999f174d1abfffdf2fc
delapria <delapria@hotmail.com>
2020-11-19 14:30:48 -0300

    Ajuste resposive leatherboard

--- src/components/ListPlays/index.js
+++ src/components/ListPlays/index.js
@@ -7,13 +7,20 @@ const ItemsTitle = styled.li`
   justify-content: space-between;
   font-size: 20px;
   width: 50%;
-  height: 20px;
   border: 2px;
   border-color: red;
   border-radius: 10px;
-  padding: 10px 10px;
+  padding: 5px;
   background-color: #ce8900;
   box-shadow: 1px 3px 20px rgba(0, 2, 94, 0.2);
+  margin: 10px 0 10px 0;
+
+  @media (max-width: 405px) {
+    font-size: 15px;
+    /* flex-direction: column;
+    justify-content: center;
+    align-items: center; */
+  }
 `;
 
 const Items = styled.li`
@@ -21,9 +28,8 @@ const Items = styled.li`
   justify-content: space-between;
   margin-top: 3px;
   width: 50%;
-  height: 15px;
-  border-radius: 10px;
-  padding: 10px 10px;
+  border-radius: 5px;
+  padding: 5px;
   background-color: white;
   box-shadow: 1px 3px 20px rgba(0, 2, 94, 0.2);
 `;
